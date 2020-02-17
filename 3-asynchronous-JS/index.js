d0c916253eaf42fbbefe11f3d1ba35cc42364186
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-02-16 23:52:52 -0400

    Chapter  Finished

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -24,8 +24,56 @@ const writeFilePro = (file, data) => {
 };
 
 const getDogPic = async () => {
-  await readFilePro(`${__dirname}/dog.txt`);
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  return '2: READY';
 };
+
+(async () => {
+  try {
+    console.log('1: Will get dog pic!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pic!');
+  } catch (err) {
+    console.log('ERROR');
+  }
+})();
+
+// Using then for handling errors
+
+// console.log('1: Will get dog pic!');
+// getDogPic()
+//   .then(x => {
+//     console.log(x);
+//     console.log('3: Done getting dog pic!');
+//   })
+//   .catch(err => {
+//     console.log('ERROR');
+//   });
+
+//
+
+//Using promises & then
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then(data => {
@@ -42,6 +90,8 @@ const getDogPic = async () => {
 //   .catch(err => {
 //     console.log(err);
 //   });
+
+//
 
 // Whithout promises
 
