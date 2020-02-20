b9e72e2608971d71b29c4148adbed88d2cbd2c25
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-02-20 14:07:10 -0400

    API : POST requests handling added

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -25,6 +27,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
