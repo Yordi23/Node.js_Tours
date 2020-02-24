21186733a11f8cce3c385f97a37e8ee6f28783f7
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-02-24 18:04:08 -0400

    Show static files example

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,8 +7,8 @@ const app = express();
 //Middlewares
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
