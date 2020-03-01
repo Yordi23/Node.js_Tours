cb469e94e2b468c00ea7947faaf2558b118da3ee
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-01 16:55:39 -0400

    Mongo and Mongoose initial implementation

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,55 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
-
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection succesful');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('Error:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
