5a5d705700308dd8670ee3027975426330d60f10
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-01 21:05:02 -0400

    Tour Model Update

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -77,7 +77,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent'
+      message: err
     });
   }
 };
