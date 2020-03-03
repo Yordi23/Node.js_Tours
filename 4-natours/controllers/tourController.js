fac0f13d390309473ee0f27a9b4d74fba367792b
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-03 10:33:58 -0400

    Added field limiting

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -56,6 +56,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // { difficulty: 'easy', duration: {$gte: 5}}
     //Execute query
     const tours = await query;
