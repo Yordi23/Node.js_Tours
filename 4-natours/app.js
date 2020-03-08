a3bc35ae053d4d6fb215e370aaf2566ba17d36ee
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-08 17:43:31 -0400

    Aggregation pipeline

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
 });
 
 //Routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -6,6 +6,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
