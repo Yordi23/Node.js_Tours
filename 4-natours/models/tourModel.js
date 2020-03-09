46216e60e6a46535e5df5a8ed95120175f259b27
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-09 18:24:04 -0400

    Added custom data validator

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal than 40 characters'],
       minlength: [10, 'A tour name must have more or equal than 10 characters']
+      //validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -42,7 +44,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          //'this' only points to current doc on NEW documents, it's not going to work while updating
+          return val < this.price;
+        },
+        message: 'Discount price {{VALUE}}should be below regular price'
+      }
+    },
     summary: {
       type: String,
       trim: true,
