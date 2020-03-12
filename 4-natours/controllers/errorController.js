b055d858d34c35de78c936a82f2e3262a9d68c3e
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-12 18:43:44 -0400

    Splitting development  and production errors responses

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,10 +1,42 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operations, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  }
+
+  //Programming or other unkown error: don't leak error details
+  else {
+    // 1) Log error
+    console.error('ERROR: ', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 //Error handling middleware (uses 4 parameters), express automatically detect it.
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
