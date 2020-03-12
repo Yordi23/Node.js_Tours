d19758e24ace4816d18bcbfd8fedde916b43f4e5
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-12 18:31:08 -0400

    Added 404 error handling

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 //Middlewares
 exports.aliasTopTours = async (req, res, next) => {
@@ -35,6 +36,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID.', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,6 +65,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   });
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID.', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -70,6 +79,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndRemove(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID.', 404));
+  }
 
   res.status(204).json({
     status: 'success',
