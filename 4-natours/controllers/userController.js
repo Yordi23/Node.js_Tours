8c3c6f463e8a74454551ef0336947743ad2c509c
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-15 22:16:57 -0400

    Test2
    Signed-off-by: Yordi Ogando <yordi.ogando@pitech.com.do>

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -8,7 +8,7 @@ exports.getAllUsers = (req, res) => {
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet implementes'
+    message: 'This route is not yet implemented'
   });
 };
 
