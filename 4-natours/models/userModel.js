d3712f51c8edb0dea0aaa236f1e0862941c000fa
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-03-28 21:41:41 -0400

    Creating user model

---
+++ 4-natours/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name.']
+  },
+  photo: String,
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email.']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minlength: [8, 'Password must have min 8 characters']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please provide confirm your password.']
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
