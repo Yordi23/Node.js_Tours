0510989b7766f208d1e1a41d30383198f764bdcd
Yordi Ogando <yordiogando@gmail.com>
2020-04-05 20:41:45 -0400

    Created getAllUsers controller

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
   res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet implemented'
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
