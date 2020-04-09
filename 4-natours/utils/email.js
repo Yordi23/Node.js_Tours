9a2352582bbcf4e7530c91b7a01a8c543e9d8f8f
Yordi Ogando <yordiogando@gmail.com>
2020-04-09 18:14:38 -0400

    Fixed nodemailer error

--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -13,13 +13,14 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Yordi Ogando <yordi.dev@gmail.com>',
+    from: 'Yordi Ogando <yordiogando@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
-    //html:
   };
 
   // 3) Send the email
   await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
