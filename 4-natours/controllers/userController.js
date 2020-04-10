ed46f58feef061736349d0fd952c64ebaaf9850d
Yordi Ogando <yordiogando@gmail.com>
2020-04-10 14:17:15 -0400

    "Delete" users donde

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -52,6 +52,17 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      data: null
+    }
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
