ab3b424e7de7901b0dc764f8a05d2b2fbb7c1c37
Yordi Ogando <yordiogando@gmail.com>
2020-04-11 18:50:26 -0400

    Prevent parameter poluution with hpp package

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +38,21 @@ app.use(mongoSanitize());
 
 //Data sanitization against XSS (malitious htmls and js)
 app.use(xss());
+
+//Prevent parameter pollution(Duplicate parameters on query strings)
+//The whitelist defines which parameters we allow duplicates
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
