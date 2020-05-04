e41c6a194e0a1a4d788d338d12dab11232cb5d73
Yordi Ogando <yordiogando@gmail.com>
2020-05-03 21:12:14 -0400

    Handler Factory Created

---
+++ 4-natours/controllers/handlerFactory.js
@@ -0,0 +1,49 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.deleteOne = Model =>
+  //Due to Javascript "closures", this inner function will have access to the variables
+  //of the outer function.
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndRemove(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID.', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID.', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const newDoc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+  });
