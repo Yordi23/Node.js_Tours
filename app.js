0b1d9a7f3c451a1c530afffeaca531535d505490
Yordi Ogando <yordiogando@gmail.com>
2020-09-20 21:02:58 -0400

    Fixed minor bug

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 
 //ROUTES
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.status(200).json({
     status: 'success',
     message: 'Node.js Tours'
